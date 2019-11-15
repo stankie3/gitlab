52aa7039e2baf5cc8b5b3bd808b70b18a4ab6c33
Keith <skankie3@go.stockton.edu>
2019-11-15 14:58:19 -0500

    Oops, we didn't want this commit

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
 	public static void main(String argv[]) {
+		//This is an unwanted but staged comment
 		String name = "World";
 		if (argv.length != 0) {
-			name = argv[0])
+			name = argv[0];
 		}
 		System.out.println("Hello, " + name + "!");
 	}
